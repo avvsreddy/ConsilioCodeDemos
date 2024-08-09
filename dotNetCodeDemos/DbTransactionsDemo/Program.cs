c24b9ce5e0f216a0bfca6795e967c17dc7cdf00c
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-08-09 09:58:21 +0530

    ado transactions demo code

--- dotNetCodeDemos/DbTransactionsDemo/Program.cs
+++ dotNetCodeDemos/DbTransactionsDemo/Program.cs
@@ -6,7 +6,12 @@ namespace DbTransactionsDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Transfering fund...");
+            bool isDone = FundTransfer(111, 222, 1000);
+            if (isDone)
+            {
+                //Console.WriteLine("Transactions is successfull");
+            }
         }
 
 
@@ -18,25 +23,40 @@ namespace DbTransactionsDemo
 
             // prepare withdraw sql update and exe
             string withdraw = $"update accounts set balance = balance - {amount} where accno={fromAccNo}";
-
+           
             SqlCommand cmd1 = new SqlCommand(withdraw, conn);
 
             // prepare deposit sql update and exe
             string deposit = $"update accounts set balance = balance + {amount} where accno={toAccNo}";
 
             SqlCommand cmd2 = new SqlCommand(deposit, conn);
-
+            conn.Open();
+            SqlTransaction trans = conn.BeginTransaction();
+            cmd1.Transaction = trans;
+            cmd2.Transaction = trans;
             try
             {
-                conn.Open();
-                cmd1.ExecuteNonQuery();
-                cmd2.ExecuteNonQuery();
+                
+                cmd1.ExecuteNonQuery();// withdraw
+                Console.WriteLine($"From {fromAccNo} amount {amount} is debited ");
+                throw new Exception("Server Error");
+                cmd2.ExecuteNonQuery();// deposit
+                Console.WriteLine($"To {toAccNo} amount {amount} is credited ");
+                trans.Commit();
+                Console.WriteLine("Transaction is successfull");
+                return true;
+            }
+            catch(Exception ex)
+            {
+                trans.Rollback();
+                Console.WriteLine("Transaction is rolled back");
+                return false;
             }
             finally 
             { 
                 conn.Close();
             }
-
+            
         }
     }
 }
